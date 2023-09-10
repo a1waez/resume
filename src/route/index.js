c4c42bb474ce62fc87801dde3c44b440183f9cd4
Max <Maksim@Dell>
2023-09-10 20:48:40 +0300

    prodniy bootstrap

--- src/route/index.js
+++ src/route/index.js
@@ -257,5 +257,23 @@ router.get('/work', function (req, res) {
     footer,
   })
 })
+
+// ================================================================
+
+// router.get Створює нам один ентпоїнт
+
+//           ↙ тут вводимо шлях (PATH) до сторінки
+router.get('/bootstrap', function (req, res) {
+  // res.render генерує нам HTML сторінку
+
+  //            ↙ cюди вводимо назву файлу з сontainer
+  res.render('bootstrap', {
+    layout: 'bootstrap',
+  })
+  //                  ↑↑ сюди вводимо JSON дані
+})
+
+// ================================================================
+
 // Підключаємо роутер до бек-енду
 module.exports = router
